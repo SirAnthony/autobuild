37346bcf6f2023ad76558d1c965e6a3f2ffd8f33
aix27249 <i27249@gmail.com>
2012-10-09 01:57:20 +0400

    add script to get installed version of specified package

---
+++ get_installed_version.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This script calls sqlite directly to find out what package version is installed
+PKGNAME=$1
+PKGVER=$(sqlite3 /var/mpkg/packages.db "SELECT package_version FROM packages WHERE package_installed=1 AND package_name=\"$PKGNAME\"")
+[ "$PKGVER" = "" ] && exit 1
+PKGBUILD=$(sqlite3 /var/mpkg/packages.db "SELECT package_build FROM packages WHERE package_installed=1 AND package_name=\"$PKGNAME\"")
+
+echo ${PKGVER}-${PKGBUILD}
+
