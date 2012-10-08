41e4b6789c205e8b52421775b9724925ab41dfaa
aix27249 <i27249@gmail.com>
2012-10-09 00:34:03 +0400

    rewrite by functions, seems to work now

--- buildorder.php
+++ buildorder.php
@@ -1,171 +1,145 @@
 #!/usr/bin/php
 <?php
 
-function isBlacklist($p) {
-	$blacklist = array("aaa_elflibs", "aaa_base", "aaa_terminfo", "aaa_elflibs_dummy");
-	foreach($blacklist as $b) {
-		if ($b == $p) return true;
-	}
-	return false;
-}
-
-function get_builddeps($abuild) {
-	echo "GET: $abuild\n";
-	if (!file_exists($abuild)) {
-		echo "GET_ERROR: No such file $abuild\n";
-		return array();
-	}
-	$handle = popen("./get_builddeps.sh $abuild", 'r');
-	$data = fread($handle, 65536);
-	pclose($handle);
-	if (trim(preg_replace("/\n/", '', $data))=="") return array();
-	$data = preg_replace("/\n/", '', trim($data));
-	$deps = explode(' ', $data);
-	$ret = array();
-	foreach($deps as $d) {
-		if (!isBlacklist($d)) $ret[] = $d;
-	}
-	return $ret;
-}
-
-function get_deps($pkgname) {
-	echo "API CALL $pkgname\n";
-	$handle = popen("wget -qO- 'http://api.agilialinux.ru/get_dep.php?n=" . urlencode($pkgname) . "'", 'r');
-	$data = fread($handle, 65536);
-	echo "RAW API DATA ($pkgname): $data\n";
-	pclose($handle);
-	if (trim(preg_replace("/\n/", '', $data))=="") return array();
-	$deps = explode("\n", trim($data));
-	$ret = array();
-	foreach($deps as $d) {
-		if (!isBlacklist($d)) $ret[] = $d;
-	}
-	return $ret;
-}
+require_once 'functions.php';
 
 
-// Init first line
-for($i=1; $i<$argc; $i++) {
-	$arg = $argv[$i];
-	echo "ARG $arg\n";
-	$d = get_builddeps('/home/aix/abuilds/' . $arg . '/ABUILD');
-	echo "SIZEOF $arg: " . sizeof($d) . "\n";
-	if (sizeof($d)==0) $d = get_deps($arg);
-	echo "API_SIZEOF $arg: " . sizeof($d) . "\n";
-	$deps[$arg] = $d;
-}
-$old_size = 0;
-$new_size = sizeof($deps);
-while ($old_size!=$new_size) {
-	echo "WHILE: $old_size != $new_size\n";
-	foreach($deps as $pkgname => $pkgdeps) {
-		foreach($pkgdeps as $p) {
-			if (isset($deps[$p])) continue;
-			$d = get_builddeps($p);
-			echo "SIZEOF $p: " . sizeof($d) . "\n";
-			if (sizeof($d)==0) $d = get_deps($p);
-			echo "API_SIZEOF $p: " . sizeof($d) . "\n";
-			$deps[$p] = $d;
-		}
+// Fill deps with command line arguments
+function initCmdline($argc, $argv) {
+	$deps = array();
+	for ($i=1; $i<$argc; $i++) {
+		$arg = $argv[$i];
+		$d = get_builddeps('/home/aix/abuilds/' . $arg . '/ABUILD');
+		if (sizeof($d)==0) $d = get_deps($arg);
+		$deps[$arg] = $d;
 	}
-	$old_size = $new_size;
+	return $deps;
+}
+
+// Expand deps: get dependencies of all dependencies and fill array with them
+function expandDeps($deps) {
+	$old_size = 0;
 	$new_size = sizeof($deps);
-}
-print_r($deps);
-
-
-// Building build order
-$build_order = array();
-foreach($deps as $pkgname => $dep ) {
-	if (sizeof($dep)==0) {
-		echo "ORDER INIT: $pkgname\n";
-		$build_order[] = $pkgname;
-	}
-}
-
-function inQueue($pkgname, $array) {
-	foreach($array as $a) {
-		if ($a===$pkgname) return true;
-	}
-	return false;
-}
-
-$old_size = 0;
-$new_size = sizeof($build_order);
-while (sizeof($build_order)<sizeof($deps)) {
-	echo "Queue size: " . sizeof($build_order) . "\n";
-	foreach($deps as $pkgname => $dep) {
-		if (inQueue($pkgname, $build_order)) continue;
-		$can_add = true;
-		foreach($dep as $d) {
-			if (!inQueue($d, $build_order)) {
-				echo "CANT ADD $pkgname: $d missing\n";
-				$can_add = false;
-				break;
+	while ($old_size!=$new_size) {
+		foreach($deps as $pkgname => $pkgdeps) {
+			foreach($pkgdeps as $p) {
+				if (isset($deps[$p])) continue;
+				$d = get_builddeps($p);
+				if (sizeof($d)==0) $d = get_deps($p);
+				$deps[$p] = $d;
 			}
 		}
-		if ($can_add) {
-			echo "ORDER ADD: $pkgname\n";
+		$old_size = $new_size;
+		$new_size = sizeof($deps);
+	}
+	return $deps;
+}
+
+
+// Initialize build order with zero-depended packages
+function initBuildOrder($deps) {
+	$build_order = array();
+	foreach($deps as $pkgname => $dep ) {
+		if (sizeof($dep)==0) {
 			$build_order[] = $pkgname;
 		}
 	}
+	return $build_order;
+}
+function resolveLoop($loop) {
+	// Read known loop file
+	$loop_resolve_data = file_get_contents('known_loop');
+	if ($loop_resolve_data===FALSE) die("FAILED TO OPEN FILE");
+	$known_loop = explode("\n", $loop_resolve_data);
 
-	// Loop detection block
-	$old_size = $new_size;
+	// Compare
+	$loop_errors = 0;
+	$known_loop_members = array();
+	foreach($known_loop as $k) {
+		$found = false;
+		foreach($loop as $pkgname => $l) {
+			if ($pkgname===$k) {
+				$found = true;
+				$known_loop_members[] = $pkgname;
+				break;
+			}
+		}
+		if (!$found) {
+			$loop_errors++;
+		}
+	}
+	return $known_loop_members;
+}
+
+function extractLoop($deps, $build_order) {
+	$loop = array();
+	foreach($deps as $pkgname => $dep) {
+		if (inQueue($pkgname, $build_order)) continue;
+		$loop[$pkgname] = $dep;
+	}
+	return $loop;
+
+}
+
+function resolve($deps, $build_order) {
+	$old_size = sizeof($build_order);
 	$new_size = sizeof($build_order);
-	if ($old_size===$new_size) {
-		$diff = sizeof($deps) - $new_size;
-		echo "Loop detected, packages in loop: $diff\n";
+	$in_queue = sizeof($deps) - sizeof($build_order);
+	$old_inqueue = $in_queue;
+	while ($in_queue>0) {
 		foreach($deps as $pkgname => $dep) {
 			if (inQueue($pkgname, $build_order)) continue;
-			$loop[$pkgname] = $dep;
-		}
-
-		// Read known loop file
-		$known_loop = explode(file_get_contents('known_loop'));
-		$loop_errors = 0;
-		foreach($loop as $l) {
-			$found = false;
-			foreach($known_loop as $k) {
-				if ($l===$k) {
-					$found = true;
+			$can_add = true;
+			foreach($dep as $d) {
+				if (!inQueue($d, $build_order)) {
+					$can_add = false;
 					break;
 				}
 			}
-			if (!$found) {
-				echo "Unknown loop with package $l\n";
-				$loop_errors++;
+			if ($can_add) {
+				$build_order[] = $pkgname;
+				$in_queue--;
 			}
 		}
-		if ($loop_errors>0) {
-			echo "Loop errors detected: $loop_errors, printing loop graph\n";
-			echo printGraph($loop, "loop");
+		$old_size = $new_size;
+		$new_size = sizeof($build_order);
+		if ($old_size===$new_size) {
+			echo "loop\n";
+			// Loop detected
+			$loop = extractLoop($deps, $build_order);
+			$loop_order = resolveLoop($loop);
+			printArray($loop_order);
+			foreach($loop_order as $pkgname) {
+				$build_order[] = $pkgname;
+				$in_queue--;
+			}
+			$new_size = sizeof($build_order);
+		}
+		if ($in_queue === $old_inqueue) {
+			echo "Queue freeze, in queue: " . $in_queue. " packages, deps: " . sizeof($deps) . ", ordered: " . sizeof($build_order) . "\n";
+			foreach($deps as $pkgname => $dep) {
+				if (inQueue($pkgname, $build_order)) continue;
+				echo "Frozen: $pkgname\n";
+			}
 			break;
 		}
-		else {
-			// Add loop twice
-			for ($i=0; $i<2; $i++) {
-				foreach($known_loop as $k) {
-					foreach($loop as $l) {
-						if ($k===$l) $build_order[] = $l;
-					}
-				}
-			}
-		}
+		$old_inqueue = $in_queue;
 	}
+	return $build_order;
 }
 
-function printGraph($loop, $fname) {
-	$data = "digraph G {\n";
-	foreach($loop as $pkgname => $dep) {
-		foreach($dep as $d) {
-			$data .= "\t\"$pkgname\" -> \"$d\";\n";
-		}
-	}
-	$data .= "}\n";
-	file_put_contents("$fname.dot", $data);
-	system("dot -Tpng -O $fname.dot");
-	return $data;
+
+function run($argc, $argv) {
+	$deps = initCmdline($argc, $argv);
+	$deps = expandDeps($deps);
+
+	$build_order = initBuildOrder($deps);
+	$build_order = resolve($deps, $build_order);
+
+	printArray($build_order);
+
+
 }
-print_r($build_order);
-echo printGraph($deps, "deps");
+
+
+run($argc, $argv);
---
+++ functions.php
@@ -0,0 +1,75 @@
+<?php
+function debug($msg) {
+	//echo $msg . "\n";
+}
+
+function printGraph($loop, $fname) {
+	$data = "digraph G {\n";
+	foreach($loop as $pkgname => $dep) {
+		foreach($dep as $d) {
+			$data .= "\t\"$pkgname\" -> \"$d\";\n";
+		}
+	}
+	$data .= "}\n";
+	file_put_contents("$fname.dot", $data);
+	system("dot -Tpng -O $fname.dot 2>/dev/null");
+	return $data;
+}
+
+
+function isBlacklist($p) {
+	$blacklist = array("aaa_elflibs", "aaa_base", "aaa_terminfo", "aaa_elflibs_dummy");
+	foreach($blacklist as $b) {
+		if ($b == $p) return true;
+	}
+	return false;
+}
+
+function inQueue($pkgname, $array) {
+	foreach($array as $a) {
+		if ($a===$pkgname) return true;
+	}
+	return false;
+}
+
+function get_builddeps($abuild) {
+	debug("GET: $abuild");
+	if (!file_exists($abuild)) {
+		debug("GET_ERROR: No such file $abuild");
+		return array();
+	}
+	$handle = popen("./get_builddeps.sh $abuild", 'r');
+	$data = fread($handle, 65536);
+	pclose($handle);
+	if (trim(preg_replace("/\n/", '', $data))=="") return array();
+	$data = preg_replace("/\n/", '', trim($data));
+	$deps = explode(' ', $data);
+	$ret = array();
+	foreach($deps as $d) {
+		if (!isBlacklist($d)) $ret[] = $d;
+	}
+	return $ret;
+}
+
+function get_deps($pkgname) {
+	debug("API CALL $pkgname");
+	$handle = popen("wget -qO- 'http://api.agilialinux.ru/get_dep.php?n=" . urlencode($pkgname) . "'", 'r');
+	$data = fread($handle, 65536);
+	debug("RAW API DATA ($pkgname): $data");
+	pclose($handle);
+	if (trim(preg_replace("/\n/", '', $data))=="") return array();
+	$deps = explode("\n", trim($data));
+	$ret = array();
+	foreach($deps as $d) {
+		if (!isBlacklist($d)) $ret[] = $d;
+	}
+	return $ret;
+}
+
+function printArray($array) {
+	echo "OUTPUT: \n";
+	foreach($array as $b) {
+		echo $b . "\n";
+	}
+
+}
