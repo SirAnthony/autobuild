4c5700d51c9c82510dedc74bbf537c6175b17050
aix27249 <i27249@gmail.com>
2012-10-09 03:58:09 +0400

    skip non-primary packages

--- findupdates.sh
+++ findupdates.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#
 # Finds abuilds for installed packages that have versions not equal to installed ones
 
 ABUILD_DIR=${ABUILD_DIR:-${HOME}/abuilds}
@@ -15,25 +16,32 @@ get_abuild_var() {
 
 for i in $(find ${ABUILD_DIR} -name ABUILD) ; do
 	ABUILD_DIRNAME=$(dirname $i)
+	ABUILD_BASENAME=$(basename ${ABUILD_DIRNAME})
 
 	ABUILD_NAME=$(get_abuild_var pkgname $i)
+
+	# Skip non-primary packages
+	if [ "${ABUILD_NAME}" != "${ABUILD_BASENAME}" ] ; then
+		continue
+	fi
+
 	ABUILD_VER=$(get_abuild_var pkgver $i)
 	ABUILD_BUILD=$(get_abuild_var pkgbuild $i)
 
-	#echo "Processing $ABUILD_NAME $ABUILD_VER-$ABUILD_BUILD"
+	# Get installed version. In future, this should refer to latest built one, not nessecarily installed
 	INSTALLED_VER=$(./get_installed_version.sh $ABUILD_NAME)
 
 	if [ "$INSTALLED_VER" == "" ] ; then
-		# Not installed
+		# No installed version, skip it
 		continue;
 	fi
 
 	if [ "$INSTALLED_VER" == "${ABUILD_VER}-${ABUILD_BUILD}" ] ; then
-		# Versions match
+		# Versions matches, skip it
 		continue;
 	fi
 
-	# If we are here, versions does not match
+	# If we are here, then versions does not match
 	if [ -z $CLEAN_OUTPUT ] ; then
 		echo "$ABUILD_NAME: $ABUILD_VER-$ABUILD_BUILD (abuild) vs $INSTALLED_VER (installed)"
 	else
