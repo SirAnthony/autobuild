8e576b07ba0548de1e499a87ec8ee27750cfd1d0
aix27249 <i27249@gmail.com>
2012-10-09 04:27:31 +0400

    skip _git, _svn, _hg part of installed version

--- get_installed_version.sh
+++ get_installed_version.sh
@@ -4,6 +4,6 @@ PKGNAME=$1
 PKGVER=$(sqlite3 /var/mpkg/packages.db "SELECT package_version FROM packages WHERE package_installed=1 AND package_name=\"$PKGNAME\"")
 [ "$PKGVER" = "" ] && exit 1
 PKGBUILD=$(sqlite3 /var/mpkg/packages.db "SELECT package_build FROM packages WHERE package_installed=1 AND package_name=\"$PKGNAME\"")
-
+PKGVER=$(echo $PKGVER | sed 's/_[git|hg|svn].*//g')
 echo ${PKGVER}-${PKGBUILD}
 
