72a5d48494d10f76ea0b1d8e7af112add9c8c229
aix27249 <i27249@gmail.com>
2012-11-14 23:25:33 +0000

    going to run full build in minimal env

--- auto_builddeps.sh
+++ auto_builddeps.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
+ABUILD_PATH=/home/aix/abuilds
 for i in $@ ; do
-	BD=$(./get_abuild_var.sh build_deps /home/aix/abuilds/$i/ABUILD)
+	FILE="$ABUILD_PATH/$i/ABUILD"
+	if [ ! -f "$FILE" ] ; then
+		echo "$i: No ABUILD"
+		continue
+	fi
+
+	BD="$(./get_abuild_var.sh build_deps $FILE)"
+	TAGS="$(./get_abuild_var.sh tags $FILE | grep virtual)"
+
+	if [ "$TAGS" != "" ] ; then
+		# Virtual packages does not have any build deps, skip it
+		continue
+	fi
+
 	if [ "$BD" == "" ] ; then
-		echo "$i has no build_deps"
+		echo "$i: no build_deps"
 	fi
 done
