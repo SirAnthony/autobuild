9f459e19c86212ec2f5f1c318abbf1a40729ee79
aix27249 <i27249@gmail.com>
2013-01-16 23:10:48 +0000

    auto_builddeps: can automatically add deps

--- auto_builddeps.sh
+++ auto_builddeps.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
+
 ABUILD_PATH=/home/aix/abuilds
+
+get_deps() {
+	# Get package deps via API
+	URL=http://api.agilialinux.ru/dep/$1
+	wget -O - $URL 2>/dev/null
+}
+
+add_deps() {
+	AB_NAME=$ABUILD_PATH/$1/ABUILD
+
+	LINENUM="$(grep -n 'tags=' $AB_NAME | sed 's/:.*//g')"
+	LINESUM=$(cat $AB_NAME | wc -l)
+	AB_TMP=${AB_NAME}.tmp
+	L=0
+	cat $AB_NAME | head -n $LINENUM | grep -v '^[^#]*build_deps=' > $AB_TMP
+	echo "" >> $AB_TMP
+	echo "# Automatically-generated build_deps. Subject to revision." >> $AB_TMP
+	echo 'build_deps="'$2'"' >> $AB_TMP
+	cat $AB_NAME | tail -n $(expr $LINESUM - $LINENUM) | grep -v '^[^#]*build_deps='  >> $AB_TMP
+	mv $AB_TMP $AB_NAME
+}
+		
+
 for i in $@ ; do
 	FILE="$ABUILD_PATH/$i/ABUILD"
 	if [ ! -f "$FILE" ] ; then
@@ -16,6 +40,11 @@ for i in $@ ; do
 	fi
 
 	if [ "$BD" == "" ] ; then
-		echo "$i: no build_deps"
+		DEPS=$(get_deps $i)
+		echo "$i: no build_deps, should be: $DEPS"
+		if [ "$AUTOADD" == "1" ] ; then
+			add_deps $i "$DEPS"
+		fi
 	fi
 done
+
